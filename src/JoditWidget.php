4f5d4766d0dd7aa921061f048e945d6eb33a66db
Chupurnov Valeriy <skoder@ya.ru>
2018-01-21 14:57:14 +0500

    New version 1.0.13. Read more http://xdsoft.net/jodit/doc/index.html#1.0.13

--- src/JoditWidget.php
+++ src/JoditWidget.php
@@ -2,55 +2,12 @@
 
 namespace yii2jodit;
 
-use yii;
 use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
 
-class JoditWidget extends InputWidget
-{
-	const PLUGIN_NAME = 'JoditEditor';
+class JoditWidget extends InputWidget {
 
-	/**
-	 * @var array
-	 * Plugins to be included, leave empty to load all plugins
-	 * <pre>sample input:
-	 * [
-	 *      //specify only needed forala plugins (local files)
-	 *      'url',
-	 *      'align',
-	 *      'char_counter',
-	 *       ...
-	 *      //override default files for a specific plugin
-	 *      'table' => [
-	 *              'css' => '<new css file url>'
-	 *          ],
-	 *      //include custom plugin
-	 *      'my_plugin' => [
-	 *              'js' => '<js file url>' // required
-	 *              'css' => '<css file url>' // optional
-	 *          ],
-	 *      ...
-	 * ]
-	 */
-	public $clientPlugins;
-
-	/**
-	 * Remove these plugins from this list plugins, this option overrides 'clientPlugins'
-	 * @var array
-	 */
-	public $excludedPlugins;
-
-	/**
-	 * JoditEditor Options
-	 * @var array
-	 */
-	public $clientOptions = [];
-
-	/**
-	 * csrf cookie param
-	 * @var string
-	 */
 	public $csrfCookieParam = '_csrfCookie';
 
 	/**
@@ -79,35 +36,12 @@ class JoditWidget extends InputWidget
 	public function registerClientScript()
 	{
 		$view = $this->getView();
-		$asset = JoditEditorAsset::register($view);
-		$asset->registerClientPlugins($this->clientPlugins, $this->excludedPlugins);
-
-		//theme
-		$themeType = isset($this->clientOptions['theme']) ? $this->clientOptions['theme'] : 'default';
-		if ($themeType != 'default') {
-			$view->registerCssFile("{$asset->baseUrl}/css/themes/{$themeType}.css", ['depends' => '\jodit\joditeditor\JoditEditorAsset']);
-		}
-		//language
-		$langType = isset($this->clientOptions['language']) ? $this->clientOptions['language'] : 'en_gb';
-		if ($langType != 'es_gb') {
-			$view->registerJsFile("{$asset->baseUrl}/js/languages/{$langType}.js", ['depends' => '\jodit\joditeditor\JoditEditorAsset']);
-		}
+		JoditAsset::register($view);
 
 		$id = $this->options['id'];
-		if (empty($this->clientPlugins)) {
-			$pluginsEnabled = false;
-		} else {
-			$pluginsEnabled = array_diff($this->clientPlugins, $this->excludedPlugins ?: []);
-		}
-		if(!empty($pluginsEnabled)){
-			foreach($pluginsEnabled as $key =>$item){
-				$pluginsEnabled[$key] = lcfirst (yii\helpers\Inflector::camelize($item));
-			}
-		}
 
-		$jsOptions = array_merge($this->clientOptions, $pluginsEnabled ? ['pluginsEnabled' => $pluginsEnabled] : []);
-		$jsOptions = Json::encode($jsOptions);
+		$jsOptions = Json::encode([]);
 
-		$view->registerJs("\$('#$id').joditEditor($jsOptions);");
+		$view->registerJs("var editor = new Jodit('#$id', $jsOptions);");
 	}
 }
