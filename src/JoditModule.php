66865585163925560d961b4bab3f6e961d08b447
Chupurnov Valeriy <skoder@ya.ru>
2018-01-22 17:05:55 +0500

    New version 1.0.25. Read more http://xdsoft.net/jodit/doc/index.html#1.0.25

--- src/JoditModule.php
+++ src/JoditModule.php
@@ -3,12 +3,113 @@ namespace yii2jodit;
 
 class JoditModule extends \yii\base\Module {
 	/**
+	 * @var bool
+	 */
+	public $debug = true; // must be true
+
+	/**
+	 * @var \Jodit\Source[]
+	 */
+	public $sources = [];
+
+	/**
+	 * @var string
+	 */
+	public $datetimeFormat = 'm/d/Y g:i A';
+
+	/**
+	 * @var int
+	 */
+	public $quality = 90;
+
+	/**
+	 * @var int
+	 */
+	public $defaultPermission = 0775;
+
+	/**
+	 * @var bool
+	 */
+	public $createThumb = true;
+
+	/**
+	 * @var string
+	 */
+	public $thumbFolderName = '_thumbs';
+
+	/**
+	 * @var string[]
+	 */
+	public $excludeDirectoryNames = ['.tmb', '.quarantine'];
+
+	/**
+	 * @var string
+	 */
+	public $maxFileSize = '8mb';
+
+	/**
+	 * @var bool
+	 */
+	public $allowCrossOrigin = false;
+
+	/**
+	 * @var array
+	 * @see https://github.com/xdan/jodit-connectors#access-control
+	 */
+	public $accessControl = [];
+
+	/**
+	 * @var string
+	 */
+	public $roleSessionVar = 'JoditUserRole';
+
+	/**
+	 * @var string
+	 */
+	public $defaultRole = 'guest';
+
+	/**
+	 * @var bool
+	 */
+	public $allowReplaceSourceFile = true;
+
+	/**
+	 * @var string
+	 */
+	public $baseurl = '';
+
+	/**
+	 * @var string
+	 */
+	public $root = '';
+
+	/**
+	 * @var string[]
+	 */
+	public $extensions = ['jpg', 'png', 'gif', 'jpeg'];
+
+	/**
+	 * @var string[]
+	 */
+	public $imageExtensions = ['jpg', 'png', 'gif', 'jpeg', 'bmp'];
+
+	/**
+	 * @var int
+	 */
+	public $maxImageWidth = 1900;
+
+	/**
+	 * @var int
+	 */
+	public $maxImageHeight = 1900;
+
+	/**
 	 * @var JoditApplication
 	 */
 	public $joditApplication;
 
 	public function init() {
-		$this->joditApplication = new JoditApplication($this->params);
+		$this->joditApplication = new JoditApplication($this);
 		parent::init();
 	}
 }
